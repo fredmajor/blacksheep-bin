f8232ecfa8e76efc4976783b899a7b27bef206e5
studio1 <studio1@studio1.local>
2017-01-31 12:08:59 +0100

    camera config update

--- camLogitechSetup.sh
+++ camLogitechSetup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-uvcdynctrl -s Exposure\ \(Absolute\) 350
+uvcdynctrl -s "Exposure, Auto" 1
+uvcdynctrl -s "Exposure (Absolute)" 350
+uvcdynctrl -s "White Balance Temperature, Auto" 0
 uvcdynctrl -s "White Balance Temperature"  5500
 uvcdynctrl -s "Gain"  0
 uvcdynctrl -s "Power Line Frequency"  1
